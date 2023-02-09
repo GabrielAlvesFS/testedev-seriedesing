3a0d16aa7329ffbbc297d4cc696a310effbbd5cb
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-08 22:27:22 -0300

    :sparkles: feat: making hamburger menu work

--- parte1/js/script.js
+++ parte1/js/script.js
@@ -1,1 +1,24 @@
-/* Coloque o JS neste arquivo */
+const menu = document.querySelector('.menu-hamburguer')
+const allCategoriesList = document.querySelector('.all-categories-list')
+const categoryItem = document.querySelectorAll('.wrapped-item')
+
+menu.addEventListener( "click", () => {
+  if (allCategoriesList.classList.contains('active')) {
+    allCategoriesList.classList.remove('active')
+  }
+  else {
+    allCategoriesList.classList.add('active')
+  }
+})
+
+// Hide menu when an item was clicked
+categoryItem.forEach( (item) => {
+  item.addEventListener( "click", () => {
+    if (allCategoriesList.classList.contains('active')) {
+      allCategoriesList.classList.remove('active')
+    }
+    else {
+      allCategoriesList.classList.add('active')
+    }
+  })
+})
