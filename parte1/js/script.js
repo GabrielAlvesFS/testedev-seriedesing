351ec844fb6783ec009ddf0aea2739caaf82936f
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-09 10:54:49 -0300

    :sparkles: feat: Carousel JS

--- parte1/js/script.js
+++ parte1/js/script.js
@@ -22,3 +22,26 @@ categoryItem.forEach( (item) => {
     }
   })
 })
+
+
+// ---- Carousel ----
+const carousel = document.querySelector(".carousel")
+const arrows = document.querySelectorAll(".carousel-section svg")
+const firstImg = carousel.querySelectorAll("img")[0]
+
+
+// showing and hiding prev and next arrow
+const showHideArrows = () => {
+  let scrollwidth = carousel.scrollWidth - carousel.clientWidth
+  arrows[0].style.display = carousel.scrollLeft == 0 ? 'none' : 'block'
+  arrows[1].style.display = carousel.scrollLeft == scrollwidth ? 'none' : 'block'
+}
+
+arrows.forEach( currArrow => {
+  currArrow.addEventListener( "click", () => {
+    let firstImgWidth = firstImg.clientWidth
+    carousel.scrollLeft += currArrow.id == "prev-btn" ? -firstImgWidth : firstImgWidth
+
+    setTimeout( () => showHideArrows(), 60)
+  })
+})
